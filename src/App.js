18ca615332319100f0eae14a8b8af28ed2268101
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2024-08-12 12:27:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Piano } from './Components/Piano';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Piano />
+      </header>
+    </div>
+  );
+}
+
+export default App;
